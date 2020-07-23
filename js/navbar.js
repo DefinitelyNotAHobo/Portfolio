27c91835fbf74279c77c8db44519986c33187bc8
Brent S <impsonsay@gmail.com>
2020-07-22 21:05:44 -0400

    cleaned up syntax

--- js/navbar.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    let x = document.getElementById("navbar");
-    if (x.className === "flex-nav") {
-      x.className += " responsive";
-    } else {
-      x.className = "flex-nav";
-    }
-  }
